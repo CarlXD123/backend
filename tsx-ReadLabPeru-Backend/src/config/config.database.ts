b3d8762b5b1c9e514f93dd3eb68f82269f491eb0
Carlos <cdx15287@gmail.com>
2022-11-04 15:58:45 -0500

    make it better

--- tsx-ReadLabPeru-Backend/src/config/config.database.ts
+++ tsx-ReadLabPeru-Backend/src/config/config.database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize"
 const db = new Sequelize('ddhnm85ruottti', 'postgres', '4ef21c6489cdd1967fb79683dc6c5008acd60779a8c908f3dda79426a2f3e1ef', {
     host: 'ec2-44-209-57-4.compute-1.amazonaws.com',//localhost
     dialect: 'postgres',
-    logging: true,
+    logging: false,
     port:5432,
     protocol: 'postgres',
     dialectOptions: {
